8309d20fd303abc17001ad9b7590f5b123329f79
Armando Salazar <armandosalazar351@gmail.com>
2024-02-11 22:13:21 -0600

    feat: add Prisma client and schema

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -9,6 +9,7 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { spawn } from 'child_process';
+import { PrismaClient } from '@prisma/client';
 
 @WebSocketGateway({
   cors: {
@@ -18,11 +19,30 @@ import { spawn } from 'child_process';
 export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
+  private prisma = new PrismaClient();
 
-  handleConnection(client: Socket) {
+  /**
+   * Called when a client connects to the server
+   * @param client ConnectedSocket
+   */
+  async handleConnection(@ConnectedSocket() client: Socket) {
+    const predictions = await this.prisma.prediction.findMany({
+      take: 10,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    // @note send the last 10 predictions to the client when they connect
+    client.emit('sensor', predictions);
+
     console.log(`[+] client connected: ${client.id}`);
   }
 
+  /**
+   * Called when a client disconnects from the server
+   * @param client ConnectedSocket
+   */
   handleDisconnect(client: Socket) {
     console.log(`[-] client disconnected: ${client.id}`);
   }
@@ -34,21 +54,37 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   ): void {
     const { humidity, temperatureCelsius } = JSON.parse(JSON.stringify(data));
 
+    // @note spawn a child process to run the python script
     const process = spawn('python3', [
       'scripts/main.py',
       humidity,
       temperatureCelsius,
     ]);
 
-    process.stdout.on('data', (data) => {
-      const dataObj = JSON.parse(JSON.stringify(data.toString()));
-      console.table(dataObj);
-      client.broadcast.emit('sensor', dataObj);
+    process.stdout.on('data', async (data) => {
+      const dataObj = eval(`(${data.toString()})`);
+
+      const prediction = await this.prisma.prediction.create({
+        data: dataObj,
+      });
+
+      console.log(prediction);
+
+      const predictions = await this.prisma.prediction.findMany({
+        take: 10,
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+
+      client.broadcast.emit('sensor', predictions);
       // console.log(`stdout: ${data}`);
     });
+
     process.stderr.on('data', (data) => {
       console.error(`stderr: ${data}`);
     });
+
     process.on('close', (code) => {
       if (code !== 0) console.log(`child process exited with code ${code}`);
     });
